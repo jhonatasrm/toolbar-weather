37f0b7ed362e96d40058b44e266be38a69df0a71
Jhonatas Rodrigues <jhonatasrm@gmail.com>
2019-01-11 22:01:05 -0200

    update version
    security update
    minor bug fixes
    strings improvements

--- src/js/background.js
+++ src/js/background.js
@@ -1,4 +1,4 @@
-var api = '';
+var api = 'f6ad51f2a88b64f68794bbb7d5665c71';
 var myStorage = window.localStorage;
 
 // geolocation
@@ -100,7 +100,6 @@ forecast.addEventListener('click', function(){
     forecast.style.display = 'none';
   }else{
     forecastPanel.style.display = 'none';
-    downArrow.style.display = 'none';
   }
 }, false);
 
@@ -130,17 +129,17 @@ upArrow.addEventListener('click', function(){
   }
 }, false);
 
-// get week day
-var weekDay = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-var d = new Date();
-var dayTomorrowResult = weekDay[d.getDay()+1];
-var dayAfterTomorrowResult = weekDay[d.getDay()+2];
+// get week day (In future Update)
+//
+//var weekDay = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+//var d = new Date();
+//var dayTomorrowResult = weekDay[d.getDay()+1];
 
-dayTomorrow.innerHTML = browser.i18n.getMessage(dayTomorrowResult); // can change for week day instead 'Tomorrow'
-dayAfterTomorrow.innerHTML = browser.i18n.getMessage(dayAfterTomorrowResult);
+dayTomorrow.textContent = browser.i18n.getMessage('tomorrow'); // can change for week day instead 'Tomorrow'
+dayAfterTomorrow.textContent = browser.i18n.getMessage('next48Hours');
 
-myStorage.setItem("dayTomorrow", browser.i18n.getMessage(dayTomorrowResult));
-myStorage.setItem("dayAfterTomorrow", browser.i18n.getMessage(dayAfterTomorrowResult));
+myStorage.setItem("dayTomorrow", browser.i18n.getMessage('tomorrow'));
+myStorage.setItem("dayAfterTomorrow", browser.i18n.getMessage('next48Hours'));
 
 //
 //save.addEventListener('click', function(){
@@ -188,25 +187,25 @@ var savedTempMaxAfterTomorrow = myStorage.getItem("tempMaxAfterTomorrow");
 
 // recover values
 imageWeather.src = savedImageWeather;
-description.innerHTML = savedDescriptionResult;
-city.innerHTML = savedCityResult;
-temperature.innerHTML = savedTemperatureResult;
-temperatureMin.innerHTML = savedTemperatureMin;
-temperatureMax.innerHTML = savedTemperatureMax;
-humidity.innerHTML = savedHumidityResult;
-wind.innerHTML = savedWindResult;
-gust.innerHTML = savedGustResult;
+description.textContent = savedDescriptionResult;
+city.textContent = savedCityResult;
+temperature.textContent = savedTemperatureResult;
+temperatureMin.textContent = savedTemperatureMin;
+temperatureMax.textContent = savedTemperatureMax;
+humidity.textContent = savedHumidityResult;
+wind.textContent = savedWindResult;
+gust.textContent = savedGustResult;
 
-weatherTomorrow.innerHTML = savedWeatherTomorrow;
-weatherAfterTomorrow.innerHTML = savedWeatherAfterTomorrow;
+weatherTomorrow.textContent = savedWeatherTomorrow;
+weatherAfterTomorrow.textContent = savedWeatherAfterTomorrow;
 imageWeatherTomorrow.src = savedImageWeatherTomorrow;
 imageWeatherAfterTomorrow.src = savedImageWeatherAfterTomorrow;
-tempMinTomorrow.innerHTML = savedTempMinTomorrow;
-tempMinAfterTomorrow.innerHTML = savedTempMinAfterTomorrow;
-tempMaxTomorrow.innerHTML = savedTempMaxTomorrow;
-tempMaxAfterTomorrow.innerHTML = savedTempMaxAfterTomorrow;
-dayTomorrow.innerHTML = savedDayTomorrow;
-dayAfterTomorrow.innerHTML = savedDayAfterTomorrow;
+tempMinTomorrow.textContent = savedTempMinTomorrow;
+tempMinAfterTomorrow.textContent = savedTempMinAfterTomorrow;
+tempMaxTomorrow.textContent = savedTempMaxTomorrow;
+tempMaxAfterTomorrow.textContent = savedTempMaxAfterTomorrow;
+dayTomorrow.textContent = savedDayTomorrow;
+dayAfterTomorrow.textContent = savedDayAfterTomorrow;
 
 //get hour
 var d = new Date();
@@ -232,26 +231,26 @@ if ((actualHour > 7)&&(actualHour < 19)){
 request.onload = function() {
 //  checkTemperature.checked = myStorage.getItem("temperatureChecked");
   var toolbarWeather = request.response;
-  city.innerHTML = toolbarWeather.city.name;
-  humidity.innerHTML = toolbarWeather.list[0].main.humidity + " %";
-  wind.innerHTML = parseInt(toolbarWeather.list[0].wind.speed * 3.6) + " km/h";
+  city.textContent = toolbarWeather.city.name;
+  humidity.textContent = toolbarWeather.list[0].main.humidity + " %";
+  wind.textContent = parseInt(toolbarWeather.list[0].wind.speed * 3.6) + " km/h";
   imageWeather.src = "../res/icons/weather_popup/"+toolbarWeather.list[0].weather[0].icon+".png";
   imageWeatherTomorrow.src = "../res/icons/weather_popup/"+toolbarWeather.list[8].weather[0].icon+".png";
   imageWeatherAfterTomorrow.src = "../res/icons/weather_popup/"+toolbarWeather.list[16].weather[0].icon+".png";
-  tempMinTomorrow.innerHTML = toolbarWeather.list[8].main.temp_min + "°C";
-  tempMaxTomorrow.innerHTML = toolbarWeather.list[8].main.temp_max + "°C";
-  tempMinAfterTomorrow.innerHTML = toolbarWeather.list[16].main.temp_min + "°C";
-  tempMaxAfterTomorrow.innerHTML = toolbarWeather.list[16].main.temp_max + "°C";
+  tempMinTomorrow.textContent = toolbarWeather.list[8].main.temp_min + "°C";
+  tempMaxTomorrow.textContent = toolbarWeather.list[8].main.temp_max + "°C";
+  tempMinAfterTomorrow.textContent = toolbarWeather.list[16].main.temp_min + "°C";
+  tempMaxAfterTomorrow.textContent = toolbarWeather.list[16].main.temp_max + "°C";
 
 //  if (checkTemperature.checked == "true"){
     updateNotification = parseInt(toolbarWeather.list[0].main.temp)+ "°C";
-    temperature.innerHTML = parseInt(toolbarWeather.list[0].main.temp) + "°C";
-    tempMin.innerHTML = toolbarWeather.list[0].main.temp_min + "°C";
-    tempMax.innerHTML = toolbarWeather.list[0].main.temp_max + "°C";
-//    tempMinTomorrow.innerHTML = toolbarWeather.main.temp_min + "°C";       // min tomorrow C
-//    tempMinAfterTomorrow.innerHTML = toolbarWeather.main.temp_min + "°C";  // min after tomorrow c
-//    tempMaxTomorrow.innerHTML = toolbarWeather.main.temp_max + "°C";       // max tomorrow C
-//    tempMaxAfterTomorrow.innerHTML = toolbarWeather.main.temp_max + "°C"; // max after tomorrow C
+    temperature.textContent = parseInt(toolbarWeather.list[0].main.temp) + "°C";
+    tempMin.textContent = toolbarWeather.list[0].main.temp_min + "°C";
+    tempMax.textContent = toolbarWeather.list[0].main.temp_max + "°C";
+//    tempMinTomorrow.textContent = toolbarWeather.main.temp_min + "°C";       // min tomorrow C
+//    tempMinAfterTomorrow.textContent = toolbarWeather.main.temp_min + "°C";  // min after tomorrow c
+//    tempMaxTomorrow.textContent = toolbarWeather.main.temp_max + "°C";       // max tomorrow C
+//    tempMaxAfterTomorrow.textContent = toolbarWeather.main.temp_max + "°C"; // max after tomorrow C
 //
     // save
     myStorage.setItem("tempMinTomorrow", toolbarWeather.list[8].main.temp_min + "°C");
@@ -264,13 +263,13 @@ request.onload = function() {
     myStorage.setItem("temperatureMax", toolbarWeather.list[0].main.temp_max + "°C");
 //  }else{
 //    updateNotification = parseInt((toolbarWeather.main.temp * 9)/5 + 32)+"°F";
-//    temperature.innerHTML = parseInt((toolbarWeather.main.temp * 9)/5 + 32) + "°F";
-//    tempMin.innerHTML = ((toolbarWeather.main.temp_min * 9)/5 + 32).toFixed(1)  + "°F";
-//    tempMax.innerHTML = ((toolbarWeather.main.temp_max * 9)/5 + 32 ).toFixed(1) + "°F";
-//    tempMinTomorrow.innerHTML = ((toolbarWeather.main.temp_min * 9)/5 + 32).toFixed(1)  + "°F"; // min tomorrow
-//    tempMaxTomorrow.innerHTML = ((toolbarWeather.main.temp_max * 9)/5 + 32).toFixed(1)  + "°F"; // min tomorrow
-//    tempMinAfterTomorrow.innerHTML = ((toolbarWeather.main.temp_min * 9)/5 + 32).toFixed(1)  + "°F"; // min tomorrow
-//    tempMaxAfterTomorrow.innerHTML = ((toolbarWeather.main.temp_max * 9)/5 + 32).toFixed(1)  + "°F"; // min tomorrow
+//    temperature.textContent = parseInt((toolbarWeather.main.temp * 9)/5 + 32) + "°F";
+//    tempMin.textContent = ((toolbarWeather.main.temp_min * 9)/5 + 32).toFixed(1)  + "°F";
+//    tempMax.textContent = ((toolbarWeather.main.temp_max * 9)/5 + 32 ).toFixed(1) + "°F";
+//    tempMinTomorrow.textContent = ((toolbarWeather.main.temp_min * 9)/5 + 32).toFixed(1)  + "°F"; // min tomorrow
+//    tempMaxTomorrow.textContent = ((toolbarWeather.main.temp_max * 9)/5 + 32).toFixed(1)  + "°F"; // min tomorrow
+//    tempMinAfterTomorrow.textContent = ((toolbarWeather.main.temp_min * 9)/5 + 32).toFixed(1)  + "°F"; // min tomorrow
+//    tempMaxAfterTomorrow.textContent = ((toolbarWeather.main.temp_max * 9)/5 + 32).toFixed(1)  + "°F"; // min tomorrow
     // save
 //    myStorage.setItem("tempMinTomorrow", ((toolbarWeather.main.temp_min * 9)/5 + 32).toFixed(1)  + "°F"); // min tomorrow
 //    myStorage.setItem("tempMinAfterTomorrow", ((toolbarWeather.main.temp_min * 9)/5 + 32).toFixed(1)  + "°F"); // min tomorrow
@@ -283,122 +282,122 @@ request.onload = function() {
 //  }
 
   if (toolbarWeather.list[0].wind.speed == null){
-    gust.innerHTML = "0 km/h";
+    gust.textContent = "0 km/h";
     var currentGust = "0 km/h";
   }else{
-    gust.innerHTML = parseInt(toolbarWeather.list[0].wind.speed * 3.6) + " km/h";
+    gust.textContent = parseInt(toolbarWeather.list[0].wind.speed * 3.6) + " km/h";
     var currentGust = parseInt(toolbarWeather.list[0].wind.speed * 3.6) + " km/h";
   }
 
   browser.browserAction.setIcon({path: "../res/icons/weather/"+toolbarWeather.list[0].weather[0].icon+".png"});
 
   if (toolbarWeather.list[0].weather[0].id == 800){
-    description.innerHTML = browser.i18n.getMessage("clearSky");
+    description.textContent = browser.i18n.getMessage("clearSky");
     var currentDescription = browser.i18n.getMessage("clearSky");
   }
   else if (toolbarWeather.list[0].weather[0].id == 801){
-    description.innerHTML = browser.i18n.getMessage("fewClouds");
+    description.textContent = browser.i18n.getMessage("fewClouds");
     var currentDescription = browser.i18n.getMessage("fewClouds");
   }
   else if (toolbarWeather.list[0].weather[0].id == 802) {
-    description.innerHTML = browser.i18n.getMessage("scatteredClouds");
+    description.textContent = browser.i18n.getMessage("scatteredClouds");
     var currentDescription = browser.i18n.getMessage("scatteredClouds");
   }
   else if ((toolbarWeather.list[0].weather[0].id >= 803) && (toolbarWeather.list[0].weather[0].id <= 804)) {
-    description.innerHTML = browser.i18n.getMessage("brokenClouds");
+    description.textContent = browser.i18n.getMessage("brokenClouds");
     var currentDescription = browser.i18n.getMessage("brokenClouds");
   }
   else if  ((toolbarWeather.list[0].weather[0].id >= 300) &&  (toolbarWeather.list[0].weather[0].id <= 321)) {
-    description.innerHTML = browser.i18n.getMessage("showerRain");
+    description.textContent = browser.i18n.getMessage("showerRain");
     var currentDescription = browser.i18n.getMessage("showerRain");
   }
   else if ((toolbarWeather.list[0].weather[0].id >= 600) &&  (toolbarWeather.list[0].weather[0].id <= 622)) {
-    description.innerHTML = browser.i18n.getMessage("snow");
+    description.textContent = browser.i18n.getMessage("snow");
     var currentDescription = browser.i18n.getMessage("snow");
   }
   else if ((toolbarWeather.list[0].weather[0].id >= 701) && (toolbarWeather.list[0].weather[0].id <= 781)) {
-    description.innerHTML = browser.i18n.getMessage("mist");
+    description.textContent = browser.i18n.getMessage("mist");
     var currentDescription = browser.i18n.getMessage("mist");
   }
   else if ((toolbarWeather.list[0].weather[0].id >= 200) && (toolbarWeather.list[0].weather[0].id <= 232)) {
-    description.innerHTML = browser.i18n.getMessage("thunderstorm");
+    description.textContent = browser.i18n.getMessage("thunderstorm");
   }
   else if ((toolbarWeather.list[0].weather[0].id >= 500) &&  (toolbarWeather.list[0].weather[0].id <= 504)) {
-    description.innerHTML = browser.i18n.getMessage("rain");
+    description.textContent = browser.i18n.getMessage("rain");
     var currentDescription = browser.i18n.getMessage("rain");
   }
 
   // tomorrow weather
   if (toolbarWeather.list[8].weather[0].id == 800){
-    weatherTomorrow.innerHTML = browser.i18n.getMessage("clearSky");
+    weatherTomorrow.textContent = browser.i18n.getMessage("clearSky");
     var currentWeatherTomorrow = browser.i18n.getMessage("clearSky");
   }
   else if (toolbarWeather.list[8].weather[0].id == 801){
-    weatherTomorrow.innerHTML = browser.i18n.getMessage("fewClouds");
+    weatherTomorrow.textContent = browser.i18n.getMessage("fewClouds");
     var currentWeatherTomorrow = browser.i18n.getMessage("fewClouds");
   }
   else if (toolbarWeather.list[8].weather[0].id == 802) {
-    weatherTomorrow.innerHTML = browser.i18n.getMessage("scatteredClouds");
+    weatherTomorrow.textContent = browser.i18n.getMessage("scatteredClouds");
     var currentWeatherTomorrow = browser.i18n.getMessage("scatteredClouds");
   }
   else if ((toolbarWeather.list[8].weather[0].id >= 803) && (toolbarWeather.list[8].weather[0].id <= 804)) {
-    weatherTomorrow.innerHTML = browser.i18n.getMessage("brokenClouds");
+    weatherTomorrow.textContent = browser.i18n.getMessage("brokenClouds");
     var currentWeatherTomorrow = browser.i18n.getMessage("brokenClouds");
   }
   else if  ((toolbarWeather.list[8].weather[0].id >= 300) &&  (toolbarWeather.list[8].weather[0].id <= 321)) {
-    weatherTomorrow.innerHTML = browser.i18n.getMessage("showerRain");
+    weatherTomorrow.textContent = browser.i18n.getMessage("showerRain");
     var currentWeatherTomorrow = browser.i18n.getMessage("showerRain");
   }
   else if ((toolbarWeather.list[8].weather[0].id >= 600) &&  (toolbarWeather.list[8].weather[0].id <= 622)) {
-    weatherTomorrow.innerHTML = browser.i18n.getMessage("snow");
+    weatherTomorrow.textContent = browser.i18n.getMessage("snow");
     var currentWeatherTomorrow = browser.i18n.getMessage("snow");
   }
   else if ((toolbarWeather.list[8].weather[0].id >= 701) && (toolbarWeather.list[8].weather[0].id <= 781)) {
-    weatherTomorrow.innerHTML = browser.i18n.getMessage("mist");
+    weatherTomorrow.textContent = browser.i18n.getMessage("mist");
     var currentWeatherTomorrow = browser.i18n.getMessage("mist");
   }
   else if ((toolbarWeather.list[8].weather[0].id >= 200) && (toolbarWeather.list[8].weather[0].id <= 232)) {
-    weatherTomorrow.innerHTML = browser.i18n.getMessage("thunderstorm");
+    weatherTomorrow.textContent = browser.i18n.getMessage("thunderstorm");
   }
   else if ((toolbarWeather.list[8].weather[0].id >= 500) &&  (toolbarWeather.list[8].weather[0].id <= 504)) {
-    weatherTomorrow.innerHTML = browser.i18n.getMessage("rain");
+    weatherTomorrow.textContent = browser.i18n.getMessage("rain");
     var currentWeatherTomorrow = browser.i18n.getMessage("rain");
   }
 
    // after tomorrow weather
   if (toolbarWeather.list[16].weather[0].id == 800){
-    weatherAfterTomorrow.innerHTML = browser.i18n.getMessage("clearSky");
+    weatherAfterTomorrow.textContent = browser.i18n.getMessage("clearSky");
     var currentWeatherAfterTomorrow = browser.i18n.getMessage("clearSky");
   }
   else if (toolbarWeather.list[16].weather[0].id == 801){
-    weatherAfterTomorrow.innerHTML = browser.i18n.getMessage("fewClouds");
+    weatherAfterTomorrow.textContent = browser.i18n.getMessage("fewClouds");
     var currentWeatherAfterTomorrow = browser.i18n.getMessage("fewClouds");
   }
   else if (toolbarWeather.list[16].weather[0].id == 802) {
-    weatherAfterTomorrow.innerHTML = browser.i18n.getMessage("scatteredClouds");
+    weatherAfterTomorrow.textContent = browser.i18n.getMessage("scatteredClouds");
     var currentWeatherAfterTomorrow = browser.i18n.getMessage("scatteredClouds");
   }
   else if ((toolbarWeather.list[16].weather[0].id >= 803) && (toolbarWeather.list[16].weather[0].id <= 804)) {
-    weatherAfterTomorrow.innerHTML = browser.i18n.getMessage("brokenClouds");
+    weatherAfterTomorrow.textContent = browser.i18n.getMessage("brokenClouds");
     var currentWeatherAfterTomorrow = browser.i18n.getMessage("brokenClouds");
   }
   else if  ((toolbarWeather.list[16].weather[0].id >= 300) &&  (toolbarWeather.list[16].weather[0].id <= 321)) {
-    weatherAfterTomorrow.innerHTML = browser.i18n.getMessage("showerRain");
+    weatherAfterTomorrow.textContent = browser.i18n.getMessage("showerRain");
     var currentWeatherAfterTomorrow = browser.i18n.getMessage("showerRain");
   }
   else if ((toolbarWeather.list[16].weather[0].id >= 600) &&  (toolbarWeather.list[16].weather[0].id <= 622)) {
-    weatherAfterTomorrow.innerHTML = browser.i18n.getMessage("snow");
+    weatherAfterTomorrow.textContent = browser.i18n.getMessage("snow");
     var currentWeatherAfterTomorrow = browser.i18n.getMessage("snow");
   }
   else if ((toolbarWeather.list[16].weather[0].id >= 701) && (toolbarWeather.list[16].weather[0].id <= 781)) {
-    weatherAfterTomorrow.innerHTML = browser.i18n.getMessage("mist");
+    weatherAfterTomorrow.textContent = browser.i18n.getMessage("mist");
     var currentWeatherAfterTomorrow = browser.i18n.getMessage("mist");
   }
   else if ((toolbarWeather.list[16].weather[0].id >= 200) && (toolbarWeather.list[16].weather[0].id <= 232)) {
-    weatherAfterTomorrow.innerHTML = browser.i18n.getMessage("thunderstorm");
+    weatherAfterTomorrow.textContent = browser.i18n.getMessage("thunderstorm");
   }
   else if ((toolbarWeather.list[16].weather[0].id >= 500) &&  (toolbarWeather.list[16].weather[0].id <= 504)) {
-    weatherAfterTomorrow.innerHTML = browser.i18n.getMessage("rain");
+    weatherAfterTomorrow.textContent = browser.i18n.getMessage("rain");
     var currentWeatherAfterTomorrow = browser.i18n.getMessage("rain");
   }
 
--- src/js/language.js
+++ src/js/language.js
@@ -1,8 +1,3 @@
-const menuCommonReplies = document.getElementById('menuCommonReplies'),
-      menuSocialSupportGuidelines = document.getElementById('menuSocialSupportGuidelines'),
-      TelegramMenu = document.getElementById('menuTelegram'),
-      SeparatorToTelegram = document.getElementById('menuJoinUS');
-
 let textElements = document.querySelectorAll('[data-manifest]');
     for (let element of textElements) {
       element.textContent = Manifest[element.dataset.manifest];
@@ -10,5 +5,5 @@ let textElements = document.querySelectorAll('[data-manifest]');
 
 textElements = document.querySelectorAll('[data-i18n]');
     for (let element of textElements) {
-      element.innerHTML = browser.i18n.getMessage(element.dataset.i18n);
+      element.textContent = browser.i18n.getMessage(element.dataset.i18n);
     }
