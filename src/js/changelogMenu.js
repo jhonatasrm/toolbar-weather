cc3334249eb42a5d645f175d2dc1db12cc5b2fa0
Jhonatas Rodrigues <jhonatasrm@gmail.com>
2019-01-20 12:31:18 -0200

    update version
    #security update
    #update v1.6

--- src/js/changelogMenu.js
+++ src/js/changelogMenu.js
@@ -1,1 +1,41 @@
-var _0xb284=["\x63\x68\x61\x6E\x67\x65\x6C\x6F\x67\x4D\x65\x6E\x75","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x63\x68\x61\x6E\x67\x65\x6C\x6F\x67\x50\x61\x6E\x65\x6C","\x75\x70\x41\x72\x72\x6F\x77","\x6D\x61\x69\x6E\x50\x61\x6E\x65\x6C","\x63\x6C\x69\x63\x6B","\x64\x69\x73\x70\x6C\x61\x79","\x73\x74\x79\x6C\x65","\x6E\x6F\x6E\x65","\x69\x6E\x6C\x69\x6E\x65","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72"];var changelogMenu=document[_0xb284[1]](_0xb284[0]);var changelogPanel=document[_0xb284[1]](_0xb284[2]);var upArrow=document[_0xb284[1]](_0xb284[3]);var mainPanel=document[_0xb284[1]](_0xb284[4]);changelogMenu[_0xb284[10]](_0xb284[5],function(){if(changelogPanel[_0xb284[7]][_0xb284[6]]=== _0xb284[8]){changelogPanel[_0xb284[7]][_0xb284[6]]= _0xb284[9];mainPanel[_0xb284[7]][_0xb284[6]]= _0xb284[9];upArrow[_0xb284[7]][_0xb284[6]]= _0xb284[9];changelogMenu[_0xb284[7]][_0xb284[6]]= _0xb284[8]}else {changelogPanel[_0xb284[7]][_0xb284[6]]= _0xb284[8]}},false);changelogPanel[_0xb284[10]](_0xb284[5],function(){if(changelogPanel[_0xb284[7]][_0xb284[6]]=== _0xb284[9]){changelogPanel[_0xb284[7]][_0xb284[6]]= _0xb284[9];upArrow[_0xb284[7]][_0xb284[6]]= _0xb284[9];changelogMenu[_0xb284[7]][_0xb284[6]]= _0xb284[8]}else {changelogPanel[_0xb284[7]][_0xb284[6]]= _0xb284[8];upArrow[_0xb284[7]][_0xb284[6]]= _0xb284[8];changelogMenu[_0xb284[7]][_0xb284[6]]= _0xb284[8]}},false);upArrow[_0xb284[10]](_0xb284[5],function(){if(changelogPanel[_0xb284[7]][_0xb284[6]]=== _0xb284[8]){changelogPanel[_0xb284[7]][_0xb284[6]]= _0xb284[8];mainPanel[_0xb284[7]][_0xb284[6]]= _0xb284[8];upArrow[_0xb284[7]][_0xb284[6]]= _0xb284[8]}else {changelogPanel[_0xb284[7]][_0xb284[6]]= _0xb284[8];upArrow[_0xb284[7]][_0xb284[6]]= _0xb284[8];changelogMenu[_0xb284[7]][_0xb284[6]]= _0xb284[9]}},false)
+// changelog menu
+var changelogMenu = document.getElementById("changelogMenu");
+var changelogPanel = document.getElementById("changelogPanel");
+var upArrow = document.getElementById("upArrow");
+var mainPanel = document.getElementById("mainPanel");
+
+changelogMenu.addEventListener('click', function(){
+  if (changelogPanel.style.display === 'none'){
+    changelogPanel.style.display = 'inline';
+    mainPanel.style.display = 'inline';
+    upArrow.style.display = 'inline';
+    changelogMenu.style.display = 'none';
+  }else{
+    changelogPanel.style.display = 'none';
+  }
+}, false);
+
+changelogPanel.addEventListener('click', function(){
+  if (changelogPanel.style.display === 'inline'){
+    changelogPanel.style.display = 'inline';
+    upArrow.style.display = 'inline';
+    changelogMenu.style.display = 'none';
+
+  }else{
+    changelogPanel.style.display = 'none';
+    upArrow.style.display = 'none';
+    changelogMenu.style.display = 'none';
+  }
+}, false);
+
+upArrow.addEventListener('click', function(){
+  if (changelogPanel.style.display === 'none'){
+    changelogPanel.style.display = 'none';
+    mainPanel.style.display = 'none';
+    upArrow.style.display = 'none';
+  }else{
+    changelogPanel.style.display = 'none';
+    upArrow.style.display = 'none';
+    changelogMenu.style.display = 'inline';
+  }
+}, false);
