9a23a9e83a3faa1b6c0f6124d252d5e2d38c3efb
Jhonatas Machado <jhonatasrm@gmail.com>
2024-03-31 02:47:05 -0300

    updates to v2.3 (#26)

--- src/js/language.js
+++ src/js/language.js
@@ -1,10 +1,23 @@
-let textElements = document.querySelectorAll('[data-manifest]');
-for (let element of textElements) {
-    element.textContent = Manifest[element.dataset.manifest];
-}
+const updateManifestText = () => {
+    const textElements = document.querySelectorAll('[data-manifest]');
+    textElements.forEach(element => {
+        const manifestKey = element.dataset.manifest;
+        if (Manifest.hasOwnProperty(manifestKey)) {
+            element.textContent = Manifest[manifestKey];
+        }
+    });
+};
 
-textElements = document.querySelectorAll('[data-i18n]');
+const updateI18nText = () => {
+    const textElements = document.querySelectorAll('[data-i18n]');
+    textElements.forEach(element => {
+        const i18nKey = element.dataset.i18n;
+        const i18nMessage = browser.i18n.getMessage(i18nKey);
+        if (i18nMessage) {
+            element.innerText = i18nMessage;
+        }
+    });
+};
 
-for (let element of textElements) {
-    element.innerText = browser.i18n.getMessage(element.dataset.i18n);
-}
+updateManifestText();
+updateI18nText();
